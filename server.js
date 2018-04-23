41f5d7a2151b61b16aa22cf829b45a69ae5b1add
Jjeekkyyllxxhhyyddee <36774748+Jjeekkyyllxxhhyyddee@users.noreply.github.com>
2018-04-23 10:34:19 -0700

    Update server.js

--- server.js
+++ server.js
@@ -208,8 +208,9 @@ if (message.author.id !== "333009840907747329") {
     if(command == "invitemepls") {
     return message.reply({embed: {color: 9198700, author: {name: client.user.username, icon_url: client.user.avatarURL }
                                   , title: "invite", description: "Invites to the bot and the server.",
-                                  fields:[{ name: "To invite me to another server:", value: "https://discordapp.com/oauth2/authorize?client_id=335336705093533698&permissions=8&scope=bot" },
-                                            { name: "And to join the support server:", value: "https://discord.gg/E2QfWKe  Have fun!" }],
+                                  fields:[{ name: "To invite me to another server:", value:"https://discordapp.com/api/oauth2/authorize?client_id=436559107680501770&permissions=8&scope=bot" },
+                                            { name: "And to join my creators host server:", value: "https://discord.gg/uauq75w  Have fun! Everyone there likes to work on cryptology
+					     !" }],
                                  timestamp: new Date(), footer: { icon_url: client.user.avatarURL, text: "© DeathBot" } } });
   }
     if(command === "test"){
