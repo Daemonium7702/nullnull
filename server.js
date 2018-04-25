84978d6971bd670b7d829ddc236f1ca506fdf490
Jjeekkyyllxxhhyyddee <36774748+Jjeekkyyllxxhhyyddee@users.noreply.github.com>
2018-04-25 10:55:35 -0700

    Update server.js

--- server.js
+++ server.js
@@ -129,11 +129,6 @@ message.channel.send(z);
       const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
     message.channel.send(randomAnswer); 
   }
-	if(command === "tts"){
-		message.tts ={tts:true};
-		message.channel.send("a tts message was successfully sent");
-	}
-		
 if(command === "cmdlist"){
 message.channel.send({embed: { color: 9198799, author: { name: client.user.username, icon_url: client.user.avatarURL }
                               , title: "CommandList", description: "Currenly, I only have a few commands, and will get more soon.", 
