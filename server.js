918281f0a79d8b997eb43607228ba70e3801f8e5
Jjeekkyyllxxhhyyddee <36774748+Jjeekkyyllxxhhyyddee@users.noreply.github.com>
2018-04-28 18:45:48 -0700

    Update server.js

--- server.js
+++ server.js
@@ -61,14 +61,7 @@ message.delete().catch(O_o=>{});
     message.channel.send(space);
   }
          if(command === "role"){
-		const Discord = require("discord.js");
-const errors = require("../utils/errors.js");
-
-module.exports.run = async (bot, message, args) => {
-
-  //!addrole @andrew Dog Person
-  if (!message.member.hasPermission("MANAGE_ROLES")) return errors.noPerms(message, "MANAGE_ROLES");
-  if (args[0] == "help") {
+		 if (args[0] == "help") {
     message.reply("Usage: !addrole <user> <role>");
     return;
   }
