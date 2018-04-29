0a967a78a6883854f28ca7c5e14b9aacddb2d298
Jjeekkyyllxxhhyyddee <36774748+Jjeekkyyllxxhhyyddee@users.noreply.github.com>
2018-04-28 18:44:36 -0700

    Update server.js

--- server.js
+++ server.js
@@ -60,6 +60,35 @@ message.delete().catch(O_o=>{});
     const space = z.replace(/ /g, "X");
     message.channel.send(space);
   }
+         if(command === "role"){
+		const Discord = require("discord.js");
+const errors = require("../utils/errors.js");
+
+module.exports.run = async (bot, message, args) => {
+
+  //!addrole @andrew Dog Person
+  if (!message.member.hasPermission("MANAGE_ROLES")) return errors.noPerms(message, "MANAGE_ROLES");
+  if (args[0] == "help") {
+    message.reply("Usage: !addrole <user> <role>");
+    return;
+  }
+  let rMember = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
+  if (!rMember) return errors.cantfindUser(message.channel);
+  let role = args.join(" ").slice(22);
+  if (!role) return message.reply("Specify a role!");
+  let gRole = message.guild.roles.find(`name`, role);
+  if (!gRole) return message.reply("Couldn't find that role.");
+
+  if (rMember.roles.has(gRole.id)) return message.reply("They already have that role.");
+  await (rMember.addRole(gRole.id));
+
+  try {
+    await rMember.send(`this was designed to make cthulus life easier. `)
+  } catch (e) {
+    console.log(e.stack);
+
+  }
+}
 	if(command === "reverse"){
 		  if (args.length < 1) {
         throw 'You must input text to be reversed!';
