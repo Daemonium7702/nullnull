35939885b0df26f1ee3c20ea306b72a55dc4f660
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-05 01:00:45 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const config = require("./config.json");
+const randomPuppy = require('random-puppy');
 client.on("ready", () => { 
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   client.user.setActivity(`on ${client.channels.size} servers`);
