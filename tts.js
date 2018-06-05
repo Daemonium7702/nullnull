8ee35925a38fdfcc20d0d82eb0bb7cd3e510aa4c
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-05 14:28:04 -0700

    Create tts.js

---
+++ tts.js
@@ -0,0 +1,11 @@
+module.exports.run = {client, message, args) => {
+if(command === "tts"){
+		const ra = args.join(" ");
+		message.channel.send(ra, {tts: true});
+	};
+  exports.conf = {
+  aliases: ['tts', 'texttospeech']
+  };
+  exports.help = {
+  name: 'tts", description: 'This turns a message into a text to speech message.'
+  };
