15f55f2be04c4e2a7be0679665bdf85f8cc4b2e7
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 15:48:15 -0700

    Update dictionary.js

--- commands/dictionary.js
+++ commands/dictionary.js
@@ -10,7 +10,7 @@ module.exports.run = async (client, message, args, tools) => {
    const embed = new Discord.RichEmbed()
     .setColor('RANDOM') 
     .setTitle(res.word) 
-    .setUrl(res.urbanURL)
+    .setURL(res.urbanURL)
     .setDescription(`**Definition:**\n*${res.definition}*\n\n**Example:**\n*${res.example}*`)
     .addField('Author', res.author, true)
     .addField('Rating', `**\`Upvotes: ${res.thumbsUp} | Downvotes: ${res.thumbsDown}\`**`) 
