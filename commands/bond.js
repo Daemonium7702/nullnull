f4ac78070c802edcb4dbbb0cf0eef7d5ec7cf7fd
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 10:13:02 -0700

    Create bond.js

---
+++ commands/bond.js
@@ -0,0 +1,31 @@
+
+module.exports.run = async (client, message, args) => {
+       if (!message.channel.nsfw) return message.reply("You can use this command only on nsfw channels!");
+
+    var subreddits = [
+      'hentaibondage',
+      'Bondage',
+      'Dominated'
+     
+      
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            const embed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .setAuthor("4k", client.user.avatarURL)
+                .setFooter("xD")
+                .setImage(url);
+            message.channel.send({
+                embed
+            });
+        })
+}
+  exports.conf = {
+  aliases: ['Bond', 'bond']
+  };
+  exports.help = {
+  name: "Bond", description: "B-b-b-bondage."
+  }
