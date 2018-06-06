bbebc67b6f17d1570e5c6cf6673b7114c720c298
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 10:14:02 -0700

    Create ass.js

---
+++ commands/ass.js
@@ -0,0 +1,31 @@
+module.exports.run = async (client, message, args) => {
+       if (!message.channel.nsfw) return message.reply("You can use this command only on nsfw channels!");
+
+    var subreddits = [
+      'facedownassup',
+      'hentaifemdom',
+      'assinthong',
+      'asstastic'
+     
+      
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            const embed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .setAuthor("4k", client.user.avatarURL)
+                .setFooter("xD")
+                .setImage(url);
+            message.channel.send({
+                embed
+            });
+        })
+}
+ exports.conf = {
+  aliases: ['Ass', 'ass']
+  };
+  exports.help = {
+  name: "Ass", description: "Shows ass."
+  }
