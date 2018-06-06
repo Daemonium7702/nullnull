b8866d74ab331d07d712327088c2609a0c97fe5b
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 14:05:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -120,18 +120,7 @@ if(command === "joinclass"){
 				       }
 	
 	//////////////////////////ASCII///////////////
-	
-    let amount = 2;
 
-    if (!isNaN(args[0])) {
-        amount = parseInt(args[0]);
-        (amount < 1) && (amount = 1);
-        (amount > 15) && (amount = 15);
-        args = args.slice(1);
-    }
-
-    message.channel.send(args.join(' '.repeat(amount / 2)).split('').join(' '.repeat(amount)));
-};
   if(command === "shrink"){
 	const mappings = (function (object) {
     let output = [];
