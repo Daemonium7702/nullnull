58d5833b170c3a7f9d3394720294007978c75158
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 10:23:00 -0700

    Create serverinfo.js

---
+++ commands/serverinfo.js
@@ -0,0 +1,19 @@
+module.exports.run = async (client, message, args) => {
+    let sicon = message.guild.iconURL;
+    let serverembed = new Discord.RichEmbed()
+    .setDescription("Server Information")
+    .setColor("#15f153")
+    .setThumbnail(sicon)
+    .addField("Server Name", message.guild.name)
+    .addField("Created On", message.guild.createdAt)
+    .addField("You Joined", message.member.joinedAt)
+    .addField("Total Members", message.guild.memberCount);
+
+    return message.channel.send(serverembed);
+  }
+  exports.conf = {
+  aliases: ['Serverinfo', 'serverinfo']
+  };
+  exports.help = {
+  name: "Serverinfo", description: "Displays info on the server."
+  }
