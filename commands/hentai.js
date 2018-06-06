12f39c93ceae438484632daac1a3d3032e24e633
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 10:15:41 -0700

    Create hentai.js

---
+++ commands/hentai.js
@@ -0,0 +1,30 @@
+ if (command === "hentai") {
+    if (!message.channel.nsfw) return message.reply("You can use this command only on nsfw channels!");
+
+    var subreddits = [
+        'HENTAI_GIF',
+      'hentaibondage ',
+      'hentaifemdom'
+     
+      
+    ]
+    var sub = subreddits[Math.round(Math.random() * (subreddits.length - 1))];
+
+    randomPuppy(sub)
+        .then(url => {
+            const embed = new Discord.RichEmbed()
+                .setColor("RANDOM")
+                .setAuthor("4k", client.user.avatarURL)
+                .setFooter("xD")
+                .setImage(url);
+            message.channel.send({
+                embed
+            });
+        })
+}
+ exports.conf = {
+  aliases: ['Hentai', 'hentai']
+  };
+  exports.help = {
+  name: "Hentai", description: "Shows Hentai."
+  }
