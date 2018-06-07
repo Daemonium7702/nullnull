dcf11657ed032834f0e449b913ffd0c166b3cf1c
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-07 00:11:57 -0700

    Update server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ let cmd;
 	const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const ffmpeg = require('ffmpeg');
-const opus = requrie('node-opus');
+const opus = require('node-opus');
 const youtube = new YouTube('AIzaSyB23US7bJ7DJvqt_qTPZaXAdy9RV2GKJxg');
 const queue = new Map();
 
