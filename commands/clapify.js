65611596ff5c9c74a3e0fede2d8835106d4c1b3f
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-07 14:28:23 -0700

    Create clapify.js

---
+++ commands/clapify.js
@@ -0,0 +1,13 @@
+const randomizeCase = word => word.split('').map(c => Math.random() > 0.5 ? c.toUpperCase() : c.toLowerCase()).join('');
+
+exports.run = (bot, message, args) => {
+    if (args.length < 1) return message.channel.send("**I need some text to clapify.** `ium clap <sentence>`")
+    message.channel.send(args.map(randomizeCase).join(':clap:'));
+}
+
+ exports.conf = {
+  aliases: ['Clapify', 'clapify']
+  };
+  exports.help = {
+  name: "Clapify", description: "Claps that text :clap:."
+  }
