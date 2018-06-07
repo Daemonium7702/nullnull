a1689d00381bb791beed56b6a0bba30e88c8a4ed
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-07 15:27:04 -0700

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -4,7 +4,7 @@ const cheerio = require('cheerio')
 
 async function googleCommand(msg, args) {
 
-	let searchMessage = await < Message > .reply('Searching... Sec.');
+	let searchMessage = await message.reply('Searching... Sec.');
 	let searchUrl = `https://www.google.com/search?q=${encodeURIComponent(msg.content)}`;
 	return snekfetch.get(searchUrl)
 		.then((result) => {
