789d0fd239214ed2cb5255d4d29712d3fcf67b3d
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 19:34:31 -0700

    Update startclass.js

--- commands/startclass.js
+++ commands/startclass.js
@@ -2,7 +2,7 @@ module.exports.run = async (client, message, args) => {
 
 	const ttt =args.join (' ');
 	message.channel.send('<@&448095670394421259>, <@&453893282628632579>');
-		message.author.reply({embed: { color: 9198799, author: { name: client.user.username, icon_url: client.user.avatarURL }
+		message.reply({embed: { color: 9198799, author: { name: client.user.username, icon_url: client.user.avatarURL }
                               , title: "Thank You!", description: "WELCOME!", 
                               fields: [{ name: "Topic:", value: ttt }],
 				        timestamp: new Date(), footer: { icon_url: client.user.avatarURL, text: "© DeathBot" }  } });
