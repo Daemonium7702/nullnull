f365eb7524665ce62cb43cb90552bba20764e045
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-07 14:01:45 -0700

    Create bash.js

---
+++ commands/bash.js
@@ -0,0 +1,21 @@
+    
+module.exports.run = async (client, message, args) => {
+    var cmd = args.join(" ").replace("bash ", "");
+            exec(cmd, (err, stdout, stderr) => {
+                if (err) {
+                    message.channel.send("err");
+                    return;
+                }
+                if (stdout != null) {
+                    message.channel.send("```\n" + stdout + "\n```")
+                }
+                if (stderr != null) {
+                    message.channel.send("```\n" + stderr + "\n```")
+                }
+            });
+              exports.conf = {
+  aliases: ['bash', 'Bash']
+  };
+  exports.help = {
+  name: "Bash", description: "Evaluates code."
+  }
