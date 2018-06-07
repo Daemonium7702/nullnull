ff4f0fd011f3156031db42183af9872fb74f2116
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-06 23:22:38 -0700

    Create line.js

---
+++ commands/line.js
@@ -0,0 +1,10 @@
+
+module.exports.run = async (client, message, args) => {
+
+message.channel.send("`~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`");
+  exports.conf = {
+  aliases: ['Line', 'line']
+  };
+  exports.help = {
+  name: "Line", description: "Makes a line."
+  }
