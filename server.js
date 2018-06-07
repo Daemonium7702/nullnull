802676336aeb9a6d5adf456701cb7c959d6a0261
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-07 15:51:58 -0700

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const translate = require('google-translate-api')
 
 
 
+
 client.on("ready", () => { 
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   client.user.setActivity(`on ${client.channels.size} servers`);
