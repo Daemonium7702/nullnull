fb0e650f0f0c20edfeaa4d2e58a1ec60160af375
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-08 00:55:51 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,13 +1,13 @@
-module.exports.run = (client, message, args) => {
+
 	const Discord = require("discord.js");
 
-exports.run = async (client, message, args, tools, con) => {
+module.exports.run = async (client, message, args, tools, con) => {
     console.log("help.js works");
         await message.author.send(`Commands: \n\n${client.commands.map(cmd => `\`${cmd.help.name}\``).join(", ")}`);
         message.channel.send("Help sent.");
     
 }
-}
+
                 exports.conf = {
   aliases: ['Help', 'help']
   };
