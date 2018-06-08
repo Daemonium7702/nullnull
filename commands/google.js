6ee0ecbfefe5217b594fb1ac4882e02143d9d9fe
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-08 14:02:37 -0700

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,7 +1,8 @@
 const GoogleSearch = require('google-search');
 var google = require('google')
 module.exports.run = (client, message, args) => {
-
+if (message.author.id != "347885325940424714"){
+ return}else{
  
 google.resultsPerPage = 3
 var nextCounter = 0
@@ -9,7 +10,7 @@ var nextCounter = 0
 google(magic, function (err, res){
   if (err) console.error(err)
  
-  for (var i = 0; i < res.links.length; ++i) {
+  for (var i = 0; i < res.links.length; i++) {
     var link = res.links[i];
     message.channel.send(link.title + ' - ' + link.href)
   }
@@ -19,6 +20,7 @@ google(magic, function (err, res){
     if (res.next) res.next()
   }
 })
+}
 }
   exports.conf = {
   aliases: ['Google', 'google']
