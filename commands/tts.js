fb1252ae628620110e34cc3a95c2db7a4b181b3d
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-08 22:50:11 -0700

    Update tts.js

--- commands/tts.js
+++ commands/tts.js
@@ -1,9 +1,16 @@
 module.exports.run = (client, message, args) => {
-
+	if(message.author.id = "347885325940424714"){
+	const ra = args.join(" ");
+		message.channel.send(ra, {tts: true});
+	}else{
+	if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Sorry, you don\'t have permission to delete or purge messages!')
+		.then(msg => msg.delete({
+			timeout: 10000
+		}));
 		const ra = args.join(" ");
 		message.channel.send(ra, {tts: true});
 	
-};
+}};
   exports.conf = {
   aliases: ['tts', 'texttospeech']
   };
