377f5b73e176b5c522ee272deaf869569812d8d0
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 03:58:40 -0700

    Create bal.js

---
+++ commands/bal.js
@@ -0,0 +1,40 @@
+const Discord = require("discord.js");
+const fs= require(fs);
+let userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
+module.exports.run = async (client, message, args) => {
+    let sender = message.author;
+    let prefix = "."
+    if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
+    if (!userData[sender.id + message.guild.id].money) userData[sender.id + message.guild.id].money = JSON;
+    fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) => {
+        if (err) console.log(err)
+    })
+    client.on("ready", () => {
+        console.log("Economy launched....")
+    });
+
+        message.channel.send({
+            embed: {
+                title: "Bank",
+                color: 0xF1C40F,
+                fields: [{
+                        name: "Account Holder",
+                        value: message.author.username,
+                        inline: true
+                    },
+                    {
+                        name: "Account Balance",
+                        value: userData[sender.id + message.guild.id].money,
+                        incline: true
+                    }
+                ],
+            }
+        })
+    }
+
+  exports.conf = {
+  aliases: ['Bal', 'bal']
+  };
+  exports.help = {
+  name: "Bal", description: "Shows balance."
+  }
