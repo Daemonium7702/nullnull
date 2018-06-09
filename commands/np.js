45d2a8ccb69fb56bacc32e456290d5e1bdc73b3c
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 04:12:53 -0700

    Create np.js

---
+++ commands/np.js
@@ -0,0 +1,29 @@
+   const YouTube = require('simple-youtube-api');
+const ytdl = require('ytdl-core');
+const ffmpeg = require('ffmpeg');
+const opus = require('node-opus');
+const youtube = new YouTube('AIzaSyB23US7bJ7DJvqt_qTPZaXAdy9RV2GKJxg');
+const queue = new Map();
+
+var servers = {};
+var prefix = '.';
+module.exports.run = async (client, message, args) => {
+    var args = message.content.substring(prefix.length).split(" ");
+    if (!message.content.startsWith(prefix)) return;
+    var searchString = args.slice(1).join(' ');
+    var url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
+    var serverQueue = queue.get(message.guild.id);
+    switch (args[0].toLowerCase()) {
+   case "np":
+            if (!serverQueue) return message.channel.send('There is nothing playing.');
+            return message.channel.send(`🎶 Now playing: **${serverQueue.songs[0].title}**`);
+            break;
+  }
+}
+exports.conf = {
+    aliases: ['Np', 'np']
+};
+exports.help = {
+    name: "Np",
+    description: "Now Playing"
+}
