3320cca544866481e1f4c8fb0f4a5ef77fdf3e17
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-08 23:44:38 -0700

    Update tts.js

--- commands/tts.js
+++ commands/tts.js
@@ -1,17 +1,9 @@
 const Discord = require("discord.js");
 module.exports.run = (client, message, args) => {
-	if(message.author.id = "347885325940424714"){
-	const ra = args.join(" ");
-		message.channel.send(ra, {tts: true});
-	}else{
-	if (!message.member.hasPermission('MANAGE_MESSAGES')) return message.channel.send('Sorry, you don\'t have permission to delete or purge messages!')
-		.then(msg => msg.delete({
-			timeout: 10000
-		}));
 		const ra = args.join(" ");
 		message.channel.send(ra, {tts: true});
 	
-}};
+		};
   exports.conf = {
   aliases: ['tts', 'texttospeech']
   };
