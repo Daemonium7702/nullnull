afc6951c0b19e937daf847c7f166ba7eb3530dce
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 04:23:00 -0700

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -20,6 +20,70 @@ module.exports.run = async (client, message, args) => {
             serverQueue.connection.dispatcher.end('Skip command has been used!');
             return undefined;
             break;
+  }
+
+    async function handleVideo(video, message, voiceChannel, playlist = false) {
+        var serverQueue = queue.get(message.guild.id);
+        console.log(video);
+        var song = {
+            id: video.id,
+            title: video.title,
+            url: `https://www.youtube.com/watch?v=${video.id}`,
+            duration: video.duration
+        };
+        if (!serverQueue) {
+            var queueConstruct = {
+                textChannel: message.channel,
+                voiceChannel: voiceChannel,
+                connection: null,
+                songs: [],
+                volume: 5,
+                playing: true
+            };
+            queue.set(message.guild.id, queueConstruct);
+
+            queueConstruct.songs.push(song);
+
+            try {
+                var connection = await voiceChannel.join();
+                queueConstruct.connection = connection;
+                play(message.guild, queueConstruct.songs[0]);
+            } catch (error) {
+                console.error(`There was an error: ${error} If it repeats, report it by doing .bugreport [Put The error here] without the []`);
+                queue.delete(message.guild.id);
+                return message.channel.send(`There was an error: ${error} If it repeats, report it by doing .bugreport [Put The error here] without the []`);
+            }
+        } else {
+            serverQueue.songs.push(song);
+            console.log(serverQueue.songs);
+            if (playlist) return undefined;
+            else return message.channel.send(` **${song.title}** has been added to the queue!`);
+        }
+        return undefined;
+    }
+
+    function play(guild, song) {
+        var serverQueue = queue.get(guild.id);
+
+        if (!song) {
+            serverQueue.voiceChannel.leave();
+            queue.delete(guild.id);
+            return;
+        }
+        console.log(serverQueue.songs);
+
+        const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+            .on('end', reason => {
+                message.channel.send('``The queue has ended.``');
+                if (reason === 'Stream is not generating quickly enough.') console.log('Song ended.');
+                else console.log(reason);
+                serverQueue.songs.shift();
+                play(guild, serverQueue.songs[0]);
+            })
+            .on('error', error => console.error(error));
+        dispatcher.setVolumeLogarithmic(serverQueue.volume / 10);
+
+        serverQueue.textChannel.send(`🎶 Start playing: **${song.title}**`);
     }
 }
 exports.conf = {
