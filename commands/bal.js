d67aebfd04b0019e188bec5334c3686106e4b08c
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 15:09:51 -0700

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -3,9 +3,10 @@ const economy= require('discord-eco');
 module.exports.run = async (client, message, args) => {
     
     let prefix = "."
-    economy.fetchBalance(message.author.id + message.guild.id).then((i) => { // economy.fetchBalance grabs the userID, finds it, and puts the data with it into i.
-         
-   const embed = new Discord.RichEmbed()
+   // Additional Tip: If you want to make the values guild-unique, simply add + message.guild.id whenever you request.
+        economy.fetchBalance(message.author.id + message.guild.id).then((i) => { // economy.fetchBalance grabs the userID, finds it, and puts the data with it into i.
+            // Lets use an embed for This
+            const embed = new Discord.RichEmbed()
                 .setDescription(`**${message.guild.name} Bank**`)
                 .setColor(0xD4AF37) // You can set any HEX color if you put 0x before it.
                 .addField('Account Holder',message.author.username,true) // The TRUE makes the embed inline. Account Holder is the title, and message.author is the value
@@ -19,7 +20,7 @@ module.exports.run = async (client, message, args) => {
 
     }
 
-});
+
 
 
     
