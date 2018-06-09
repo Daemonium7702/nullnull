5b39f73ea398e88b14b89efe945f0b4fdb731f16
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 15:08:07 -0700

    Create add.js

---
+++ commands/add.js
@@ -0,0 +1,45 @@
+    const Discord = require('discord.js');
+const economy = require('discord-eco');
+const modRole = 'Administrator';
+module.exports.run = async (client, message, args) => {
+
+    
+    // Check if they have the modRole
+        if (!message.member.roles.find("name", modRole)) { // Run if they dont have role...
+            message.channel.send('**You need the role `' + modRole + '` to use this command...**');
+            return;
+        }
+
+        // Check if they defined an amount
+        if (!args[0]) {
+            message.channel.send(`**You need to define an amount. Usage: ${prefix}BALSET <amount> <user>**`);
+            return;
+        }
+
+        // We should also make sure that args[0] is a number
+        if (isNaN(args[0])) {
+            message.channel.send(`**The amount has to be a number. Usage: ${prefix}BALSET <amount> <user>**`);
+            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
+        }
+
+        // Check if they defined a user
+        let defineduser = '';
+        if (!args[1]) { // If they didn't define anyone, set it to their own.
+            defineduser = message.author.id;
+        } else { // Run this if they did define someone...
+            let firstMentioned = message.mentions.users.first();
+            defineduser = firstMentioned.id;
+        }
+
+        // Finally, run this.. REMEMBER IF you are doing the guild-unique method, make sure you add the guild ID to the end,
+        economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
+            message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
+        });
+
+    }
+      exports.conf = {
+  aliases: ['Add', 'add']
+  };
+  exports.help = {
+  name: "Add", description: "Money money money."
+  }
