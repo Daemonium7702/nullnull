37f1385a387ec74278acd99fb6d3b8888a6cd3dd
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 23:16:35 -0700

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -9,7 +9,7 @@ const client = new Discord.Client();
 // JSON Files
 const items = JSON.parse(fs.readFileSync('items.json', 'utf8'));
     let prefix = '.';
-    if (message.startsWith(`${prefix}BUY`)) { // We need to make a JSON file that contains the items
+module.exports.run = async (client, message, args) => {
 
         // Variables
         let categories = []; // Lets define categories as an empty array so we can add to it.
