0f9e9f3ddce0fd7657410ab72ff7e9cd267f8ba1
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-10 00:13:16 -0700

    Update bal.js

--- commands/bal.js
+++ commands/bal.js
@@ -3,13 +3,21 @@ const economy= require('discord-eco');
 module.exports.run = async (client, message, args) => {
     
     let prefix = "."
+      let defineduser = '';
+        if (!args[1]) { // If they didn't define anyone, set it to their own.
+            defineduser = message.author.id;
+        } else { // Run this if they did define someone...
+            let firstMentioned = message.mentions.users.first();
+            defineduser = firstMentioned.id;
+        }
+
    // Additional Tip: If you want to make the values guild-unique, simply add + message.guild.id whenever you request.
-        economy.fetchBalance(message.author.id + message.guild.id).then((i) => { // economy.fetchBalance grabs the userID, finds it, and puts the data with it into i.
+        economy.fetchBalance(defineduser + message.guild.id).then((i) => { // economy.fetchBalance grabs the userID, finds it, and puts the data with it into i.
             // Lets use an embed for This
             const embed = new Discord.RichEmbed()
                 .setDescription(`**${message.guild.name} Bank**`)
                 .setColor(0xD4AF37) // You can set any HEX color if you put 0x before it.
-                .addField('Account Holder',message.author.username,true) // The TRUE makes the embed inline. Account Holder is the title, and message.author is the value
+                .addField('Account Holder',defineduser,true) // The TRUE makes the embed inline. Account Holder is the title, and message.author is the value
                 .addField('Account Balance',i.money,true)
 
 
