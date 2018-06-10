67e648ea0d7764300d801081a0fbbb0d7c7706af
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 22:08:38 -0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,17 +1,19 @@
+let timer = {};
+const Discord = require('discord.js');
 const economy = require('discord-eco');
-const ms = require('parse-ms');
+
 module.exports.run = async (client, message, args, tools )=> {
-let cooldown = 8.64e+7
-let amount = 250;
-let lastDaily = await db.fetch(`lastDaily_${message.author.id}`);
-if(lastDaily !== null && cooldown - (Date.now()-lastDaily >0)){
-let timeObj = ms(cooldown - (Date.now() -lastDaily));
-message.reply(`you have already collected their daily allowance. please wait **${timeObj.hours}h-${timeObj.minutes}m**`);
-}else{message.reply(`Successfully collected $${amount}`);
-economy.set(`lastDaily_${message.author.id}`, Date.now());
-economy.add(`userBalance_${message.author.id}, 250`);
-}
-}
+        if(timer[message.author.id] === 1) {
+          message.channel.send('UOH OH. I am pretty sure that you have already collected your allowance')
+        } else {
+          message.channel.send('You have collected your allowance. Congrats')
+          economy.updateBalance(message.author.id, 50)
+            timer[message.author.id] = 1
+     
+          setTimeout(function(){
+            timer[message.author.id] = 0
+          }, 300000);
+        }
   exports.conf = {
   aliases: ['Daily', 'daily']
   };
