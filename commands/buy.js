216df9d695fed975628fb7a7e0b522c8d85ed71f
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 23:02:17 -0700

    Update buy.js

--- commands/buy.js
+++ commands/buy.js
@@ -1,29 +1,105 @@
- const Discord = require('discord.js');
+const Discord = require('discord.js');
 const economy = require('discord-eco');
+const fs = require('fs'); // Make sure you call the fs package.
 
-module.exports.run = async (client, message, args) => {
-switch (args) {
-        case 'cooldued':
-          economy.fetchBalance(message.author.id).then ((i) => {
-          let role = message.guild.roles.find("name", "cool dude")
-         
-          if (`${i.money}` >= 5000) {
-            if (!message.member.roles.find('name', "cool dued")) {
-              economy.updateBalance(message.author.id, -5000)
-              message.channel.send('You just gave us your hard earned money!')
-              message.member.addRole(role)
-            } else {
-              message.channel.send('Why are you gonna buy somethin you dont need?')
+// Define client for Discord
+const client = new Discord.Client();
+// We have to define a moderator role, the name of a role you need to run certain commands
+
+// JSON Files
+const items = JSON.parse(fs.readFileSync('items.json', 'utf8'));
+    let prefix = '.';
+    if (msg.startsWith(`${prefix}BUY`)) { // We need to make a JSON file that contains the items
+
+        // Variables
+        let categories = []; // Lets define categories as an empty array so we can add to it.
+
+        // We want to make it so that if the item is not specified it shows a list of items
+        if (!args.join(" ")) { // Run if no item specified...
+
+            // First, we need to fetch all of the categories.
+            for (var i in items) { // We can do this by creating a for loop.
+
+                // Then, lets push the category to the array if it's not already in it.
+                if (!categories.includes(items[i].type)) {
+                    categories.push(items[i].type)
+                }
+
             }
-            } else {
-              message.channel.send("You are Broke")
+
+            // Now that we have the categories we can start the embed
+            const embed = new Discord.RichEmbed()
+                .setDescription(`Available Items`)
+                .setColor(0xD4AF37)
+
+            for (var i = 0; i < categories.length; i++) { // This runs off of how many categories there are. - MAKE SURE YOU DELETE THAT = IF YOU ADDED IT.
+
+                var tempDesc = '';
+
+                for (var c in items) { // This runs off of all commands
+                    if (categories[i] === items[c].type) {
+
+                        tempDesc += `${items[c].name} - $${items[c].price} - ${items[c].desc}\n`; // Remember that \n means newline
+
+                    }
+
+                }
+
+                // Then after it adds all the items from that category, add it to the embed
+                embed.addField(categories[i], tempDesc);
+
             }
-          })
-              break;
-        default:
-          message.channel.send('```cooldued: 5000```')
-       };
-       }
+
+            // Now we need to send the message, make sure it is out of the for loop.
+            return message.channel.send({
+                embed
+            }); // Lets also return here.
+
+            // Lets test it! x2
+
+        }
+
+        // Buying the item.
+
+        // Item Info
+        let itemName = '';
+        let itemPrice = 0;
+        let itemDesc = '';
+
+        for (var i in items) { // Make sure you have the correct syntax for this.
+            if (args.join(" ").trim().toUpperCase() === items[i].name.toUpperCase()) { // If item is found, run this...
+                itemName = items[i].name;
+                itemPrice = items[i].price;
+                itemDesc = items[i].desc;
+            }
+        }
+
+        // If the item wasn't found, itemName won't be defined
+        if (itemName === '') {
+            return message.channel.send(`**Item ${args.join(" ").trim()} not found.**`)
+        }
+
+        // Now, lets check if they have enough money.
+        economy.fetchBalance(message.author.id + message.guild.id).then((i) => { // Lets fix a few errors - If you use the unique guild thing, do this.
+            if (i.money <= itemPrice) { // It's supposed to be like this instead...
+
+                return message.channel.send(`**You don't have enough money for this item.**`);
+            }
+
+            economy.updateBalance(message.author.id + message.guild.id, parseInt(`-${itemPrice}`)).then((i) => {
+
+                message.channel.send('**You bought ' + itemName + '!**');
+
+                // You can have IF statements here to run something when they buy an item.
+                if (itemName === 'Test') {
+                    message.guild.members.get(message.author.id).addRole(message.guild.roles.find("name", "Test")); // For example, when they buy the helper role it will give them the helper role.
+                }
+
+            })
+
+        })
+
+    }
 exports.conf = {
   aliases: ['Buy', 'buy']
   };
