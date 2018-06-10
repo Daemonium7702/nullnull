c169a515e8db1ffdba3c122058411d01de57a605
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-10 00:31:40 -0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -14,8 +14,8 @@ module.exports.run = async (client, message, args, tools )=> {
           message.channel.send('Sorry! I am pretty sure that you have already collected your allowance!')
         } else {
           message.channel.send('You have collected your daily allowance of $50')
-           economy.updateBalance(defineduser + message.guild.id, parseInt(args[0])).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
-            message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
+           economy.updateBalance(defineduser + message.guild.id, parseInt(50)).then((i) => { // AND MAKE SURE YOU ALWAYS PARSE THE NUMBER YOU ARE ADDING AS AN INTEGER
+            message.channel.send(`**<@${defineduser}> had $50 added/subtraction from their account.**`)
         });
      timer[message.author.id] = 1
      
