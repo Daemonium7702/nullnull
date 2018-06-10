5d48c3d6165570a0aaeca60a0f7578e3a06300f5
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 22:01:01 -0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,4 +1,4 @@
-const db = require("quick-db");
+const economy = require('discord-eco');
 const ms = require('parse-ms');
 module.exports.run = async (client, message, args, tools )=> {
 let cooldown = 8.64e+7
@@ -8,8 +8,8 @@ if(lastDaily !== null && cooldown - (Date.now()-lastDaily >0)){
 let timeObj = ms(cooldown - (Date.now() -lastDaily));
 message.reply(`you have already collected their daily allowance. please wait **${timeObj.hours}h-${timeObj.minutes}m**`);
 }else{message.reply(`Successfully collected $${amount}`);
-db.set(`lastDaily_${message.author.id}`, Date.now());
-db.add(`userBalance_${message.author.id}, 250`);
+economy.set(`lastDaily_${message.author.id}`, Date.now());
+economy.add(`userBalance_${message.author.id}, 250`);
 }
 }
   exports.conf = {
