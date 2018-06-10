e96e1114cebda94de5ea65bc32c944ee2151568f
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 21:35:05 -0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,43 +1,20 @@
-const fs = require('fs')
-const moment = require('moment');
-
-let userData = JSON.parse(fs.readFileSync('Storage/userData.json', 'utf8'));
-module.exports.run = async (client, message, args) => {
-    let sender = message.author;
-    let prefix = "."
-    if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
-    if (!userData[sender.id + message.guild.id].money) userData[sender.id + message.guild.id].money = 1000;
-    if (!userData[sender.id + message.guild.id].lastDaily) userData[sender.id + message.guild.id].lastDaily = 'Not Collected';
-    fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) => {
-        if (err) console.error(err)
-    })
-
-        if (userData[sender.id + message.guild.id].lastDaily != moment().format('L')) {
-            userData[sender.id + message.guild.id].lastDaily = moment().format('L');
-            userData[sender.id + message.guild.id].money += 250
-            message.channel.send({
-                embed: {
-                    title: "Daily Reward",
-                    description: `You got 250 Dollars as your daily reward!`
-                }
-            })
-        } else {
-            message.channel.send({
-                embed: {
-                    title: "Daily Reward",
-                    description: "Youve already collected your daily reward! Collect your next one in " + moment().endOf('day').fromNow + '.'
-                }
-            })
-        }
-    }
-    fs.writeFile("Storage/userData.json", JSON.stringify(userData), (err) => {
-        if (err) console.error(err)
-    });
-
-exports.conf = {
-    aliases: ['Daily', 'daily']
-};
-exports.help = {
-    name: "Daily",
-    description: "Daily Money."
+const db = require('quick-db');
+const ms = require('parse-ms');
+module.exports.run = async (client, message, args, tools )=> {
+let cooldown = 8.64e+7
+let amount = 250;
+let lastDaily = await db.fetch(`lastDaily_${message.author.id}`);
+if(lastDaily !== null && cooldown - (Date.now()-lastDaily >0)){
+let timeObj = ms(cooldown - (Date.now() -lastDaily));
+message.reply(`you have already collected their daily allowance. please wait **${timeObj.hours}h-${timeObj.minutes}m**`);
+}else{message.reply(`Successfully collected $${amount}`);
+db.set(`lastDaily_${message.author.id}`, Date.now());
+db.add(`userBalance_${message.author.id}, 250`);
 }
+}
+  exports.conf = {
+  aliases: ['Daily', 'daily']
+  };
+  exports.help = {
+  name: "Daily", description: "Daily bonus!!!."
+  }
