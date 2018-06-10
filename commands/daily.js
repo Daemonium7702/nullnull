2f7094a93883f6cc675fb4aea8cdd0cd931cbd1d
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-09 21:45:15 -0700

    Update daily.js

--- commands/daily.js
+++ commands/daily.js
@@ -1,4 +1,4 @@
-const db = require('quick-db');
+const db = require("quick-db");
 const ms = require('parse-ms');
 module.exports.run = async (client, message, args, tools )=> {
 let cooldown = 8.64e+7
