0163ea2651bdd3f59d6693612de24f3056c20c6c
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-11 11:29:20 -0700

    Update google.js

--- commands/google.js
+++ commands/google.js
@@ -1,10 +1,7 @@
 const GoogleSearch = require('google-search');
 var google = require('google')
 module.exports.run = (client, message, args) => {
-    if (message.guild.id = "343572980351107077") {
-        message.channel.send("this command was disabled for this server");
-        return
-    } else {
+
 
         google.resultsPerPage = 3
         var nextCounter = 0
