a6000ff94e331b0b849b8dab386c029bcdd9a1f2
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-11 07:36:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ client.on("message", async message => {
 
 
     if (message.author.bot) return;
-    if  message.content.indexOf(config.prefix) !== 0) return;
+    if  (message.content.indexOf(config.prefix) !== 0) return;
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     let msg = message.content.toLowerCase() || message.content.toUpperCase();
