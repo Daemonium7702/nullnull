4334ef49e13a3b4fe15e6540215c69913d004cb7
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-11 10:19:53 -0700

    Update addc.js

--- commands/addc.js
+++ commands/addc.js
@@ -1,34 +1,25 @@
 const sql = require("sqlite");
-sql.open("./score.sqlite");
-
-module.exports.run = async (client, message, args) => {
-    if (message.member.id != "347885325940424714") { // Run if they dont have role...
-            message.channel.send('This command can only be used by the BotCreator for the moment.');
-            return;
-        }
-
-        if (!args[0]) {
-            message.channel.send(`**You need to define an amount. Usage: ${config.prefix}add <amount> <user>**`);
-            return;
-        }
-
-        // We should also make sure that args[0] is a number
-        if (isNaN(args[0])) {
-            message.channel.send(`**The amount has to be a number. Usage: ${config.prefix}add <amount> <user>**`);
-            return; // Remember to return if you are sending an error message! So the rest of the code doesn't run.
-        }
-        let defineduser = '';
-        if (!args[1]) { // If they didn't define anyone, set it to their own.
-            defineduser = message.author.id;
-        } else { // Run this if they did define someone...
-            let firstMentioned = message.mentions.users.first();
-            defineduser = firstMentioned.id;
-        }
-        sql.run(`UPDATE money SET cash = ${row.cash + args[0]} WHERE userId = ${defineduser}`);
-        message.channel.send(`**User defined had ${args[0]} added/subtraction from their account.**`)
-
-
+ sql.open("./score.sqlite");
+ module.exports.run = async (client, message, args) => {
+     let defineduser = '';
+     if (!args[1]) { // If they didn't define anyone, set it to their own.
+         defineduser = message.author.id;
+     } else { // Run this if they did define someone... 
+         let firstMentioned = message.mentions.users.first();
+         defineduser = firstMentioned.id}
+    sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO money (userId, cash, bank) VALUES (?, ?, ?)", [defineduser, 1, 0]);
+    } else {
+      sql.run(`UPDATE money SET cash = ${row.cash + args[0]} WHERE userId = ${defineduser}`);
     }
+ exports.conf = {
+     aliases: ['Balb', 'balb']
+ };
+ exports.help = {
+     name: "Balb",
+     description: "Money money money."
+ }
           exports.conf = {
   aliases: ['Addc', 'addc']
   };
