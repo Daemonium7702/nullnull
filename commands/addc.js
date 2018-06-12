3db8c50530d41e64799fa7aa2ab965886aaa4a25
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-11 23:24:12 -0700

    Update addc.js

--- commands/addc.js
+++ commands/addc.js
@@ -1,4 +1,4 @@
-onst Discord = require("discord.js");
+const Discord = require("discord.js");
 const sql = require("sqlite");
 sql.open("./score.sqlite");
  module.exports.run = async (client, message, args) => {
