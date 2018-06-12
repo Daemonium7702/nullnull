10c6a54cee01a23d92a0fc2dc0b1335a4616486d
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-11 23:38:04 -0700

    Update balb.js

--- commands/balb.js
+++ commands/balb.js
@@ -1,4 +1,4 @@
- const sql = require("sqlite");
+const sql = require("sqlite");
  sql.open("./score.sqlite");
  module.exports.run = async (client, message, args) => {
      let defineduser = '';
@@ -7,16 +7,15 @@
      } else { // Run this if they did define someone... 
          let firstMentioned = message.mentions.users.first();
          defineduser = firstMentioned.id}
-         sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
+        sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
              if (!row) return message.reply("Your current level is 0");
              message.reply(`Your current banked money is $${row.bank}`);
+},
              sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
                  if (!row) return message.reply("sadly you do not have any points yet!");
                  message.reply(`you currently have $${row.cash} in your wallet, good going!`);
-             });
-         })
-     }
- 
+             })
+         )}
  exports.conf = {
      aliases: ['Balb', 'balb']
  };
