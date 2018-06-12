f564b05e64368206758c5a6b42ed11917f0d5256
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-12 00:18:26 -0700

    Update addc.js

--- commands/addc.js
+++ commands/addc.js
@@ -2,19 +2,34 @@ const Discord = require("discord.js");
 const sql = require("sqlite");
 sql.open("./score.sqlite");
  module.exports.run = async (client, message, args) => {
-     let defineduser = '';
-     if (!args[1]) { // If they didn't define anyone, set it to their own.
-         defineduser = message.author.id;
-     } else { // Run this if they did define someone... 
-         let firstMentioned = message.mentions.users.first();
-         defineduser = firstMentioned.id}
-    sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
+    let defineduser = message.mentions.users.first() || message.author;
+defineduser = defineduser.id;
+  if (args[2]) {
+   
+   
+      sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
+    if (!row) {
+      sql.run("INSERT INTO money (userId, cash, bank) VALUES (?, ?, ?)", [defineduser, 1, 0]);
+    } else {
+      sql.run(`UPDATE money SET cash = ${row.cash + args[2]} WHERE userId = ${defineduser}`);
+     message.channel.send(`<@${defineduser}>'s was given ${args[1]} dollars!`) }
+    });//add money with ${row.cash + args[2]}
+   
+   
+} else {
+    
+ 
+   sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
     if (!row) {
       sql.run("INSERT INTO money (userId, cash, bank) VALUES (?, ?, ?)", [defineduser, 1, 0]);
     } else {
       sql.run(`UPDATE money SET cash = ${row.cash + args[1]} WHERE userId = ${defineduser}`);
      message.channel.send(`<@${defineduser}>'s was given ${args[0]} dollars!`) }
     });
+ 
+ //add money with ${row.cash + args[1]}
+}
+  
     }
 
           exports.conf = {
