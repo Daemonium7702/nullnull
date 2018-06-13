48e6400a777ce5a907f4e8af2174229e06461122
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-12 17:10:39 -0700

    Create ban.js

---
+++ commands/ban.js
@@ -0,0 +1,26 @@
+module.exports.run = async (client, message, args) => {
+
+case "ban":
+            if(!message.guild.member(message.author).hasPermission("BAN_MEMBERS")) return message.reply("It appears you don't have permission to do this.")
+            if(!message.guild.member(bot.user).hasPermission("BAN_MEMBERS")) return message.reply("It appears I don't have permission to do this.")
+            
+            let user1 = message.mentions.users.first();
+            let reason1 = message.content.split(" ").slice(2).join(" ");
+            let modlog1 = bot.channels.find("name", "mod-log" || "moderation-log");
+
+            if(!modlog1) return message.reply("I've detected that this server doesn't have a mod-log channel.");
+            if(message.mentions.users.size < 1) return message.reply("You must mention someone in order to ban them!");
+            if(!reason1) return message.reply("Enter a reason for ban.");
+            if(!message.guild.member(user1).kickable) return message.reply("I can't ban the owner!");
+            
+            message.guild.member(user1).ban(reason);
+            
+            
+            break;
+        }
+         exports.conf = {
+  aliases: ['Ban', 'ban']
+  };
+  exports.help = {
+  name: "Ban", description: "bans"
+  }
