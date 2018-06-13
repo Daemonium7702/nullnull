d33b74db37562fa0ee550710bd3335a493a97095
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-12 22:41:41 -0700

    Update mute.js

--- commands/mute.js
+++ commands/mute.js
@@ -13,7 +13,7 @@ let time = params[1];
 if(!time) return message.reply("Cool, so since you didnt define time, Ill just mute this memebr for 0 seconds.");
 member.addRole(muteRole.id);
 modlog1.channel.send(`Nice! You've been muted for ${ms(ms(time), {long: true})} ${member.user.tag}`);
-setTimeour(function(){
+setTimeout(function(){
 member.removeRole(mute.id);
 modlog1.channel.send(`${message.member.tag} Hello m8 you have been unmuted ~~about damn time~~. It lasted: ${ms(ms(time), {long: true})}`);
 }, ms(time));
