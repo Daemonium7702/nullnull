83bb641120b4a081d861591e124099447fa9f08e
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-12 22:43:44 -0700

    Create schedule.js

---
+++ commands/schedule.js
@@ -0,0 +1,34 @@
+const ms = require("ms");
+module.exports.run = async (client, message, args, levels) => {
+jj = args.join(' ')
+let Timer = args[0];
+
+if(!args[0]){
+  return message.channel.send("Please enter a period of time, with either `s,m or h` at the end!");
+}
+
+if(args[0] <= 0){
+  return message.channel.send("Please enter a period of time, with either `s,m or h` at the end!");
+}
+
+message.channel.send(":white_check_mark: Timer has been set for: " + `${ms(ms(Timer), {long: true})}`)
+
+setTimeout(function(){
+  message.channel.send(`Timer has ended, it lasted: ${ms(ms(Timer), {long: true})} Message will now be sent` + message.author.toString());
+  message.channel.send(jj)
+
+}, ms(Timer));
+}
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User"
+};
+
+exports.help = {
+  name: "schedule",
+  category: "Miscelaneous",
+  description: "Sets a timer",
+  usage: "timer"
+};
