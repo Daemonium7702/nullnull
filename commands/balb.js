bf085d7dc987ee4730be5454a41274722969b8ae
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-13 19:23:49 -0700

    Update balb.js

--- commands/balb.js
+++ commands/balb.js
@@ -1,22 +1,32 @@
  const sql = require("sqlite");
  sql.open("./score.sqlite");
  module.exports.run = async (client, message, args) => {
-     let defineduser = '';
-     if (!args[1]) { // If they didn't define anyone, set it to their own.
-         defineduser = message.author.id;
-     } else { // Run this if they did define someone... 
-         let firstMentioned = message.mentions.users.first();
-         defineduser = firstMentioned.id}
-        sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
-             if (!row) return message.reply("Your current level is 0");
-             message.channel.send(`Current banked money of user is $${row.bank}`);
-             message.channel.send(`User currently have $${row.cash} in your wallet, good going!`);
-//},
-	     //sql.get(`SELECT * FROM money WHERE userId ="${defineduser}"`).then(row => {
-             //    if (!row) return message.reply("sadly you do not have any points yet!");
-             //})
-         })
-};
+
+
+     if (message.mentions.users.size > 1) {
+         return
+     } else {
+         sql.get(`SELECT * FROM money WHERE userId ="${message.mentions.users.id}"`).then(row => {
+                 if (!row) return message.reply("Your current level is 0");
+                 message.channel.send(`Current banked money of user is $${row.bank}`);
+                 message.channel.send(`User currently have $${row.cash} in your wallet, good going!`);
+
+             }
+
+         )
+     }
+
+
+
+     if (!message.mentions.user.size) {
+            sql.get(`SELECT * FROM money WHERE userId ="${message.author.id}"`).then(row => {
+                 if (!row) return message.reply("Your current level is 0");
+                 message.channel.send(`Current banked money of user is $${row.bank}`);
+                 message.channel.send(`User currently have $${row.cash} in your wallet, good going!`);
+
+             }
+     }
+ }
  exports.conf = {
      aliases: ['Balb', 'balb']
  };
