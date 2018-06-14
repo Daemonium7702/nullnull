91701a70daa29715d83a4505e97b2205518001b4
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-14 12:29:13 -0700

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,8 +1,9 @@
+const client = new Discord.Client();
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 const ffmpeg = require('ffmpeg');
 const opus = require('node-opus');
-const queue = new Map();
+client.queue = new Map();
 const youtube = new YouTube('AIzaSyB23US7bJ7DJvqt_qTPZaXAdy9RV2GKJxg');
 var prefix = '.';
 module.exports.run = async (client, message, args) => {
@@ -10,7 +11,7 @@ module.exports.run = async (client, message, args) => {
     if (!message.content.startsWith(prefix)) return;
     var searchString = args.slice(1).join(' ');
     var url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-    var serverQueue = queue.get(message.guild.id);
+    var serverQueue = client.queue.get(message.guild.id);
     switch (args[0].toLowerCase()) { 
  case "queue":
             if (!serverQueue) return message.channel.send('There is nothing playing.');
@@ -23,7 +24,7 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
         }
 
     async function handleVideo(video, message, voiceChannel, playlist = false) {
-        var serverQueue = queue.get(message.guild.id);
+        var serverQueue = client.queue.get(message.guild.id);
         console.log(video);
         var song = {
             id: video.id,
@@ -40,7 +41,7 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
                 volume: 5,
                 playing: true
             };
-            queue.set(message.guild.id, queueConstruct);
+            client.queue.set(message.guild.id, queueConstruct);
 
             queueConstruct.songs.push(song);
 
@@ -50,7 +51,7 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
                 play(message.guild, queueConstruct.songs[0]);
             } catch (error) {
                 console.error(`There was an error: ${error} If it repeats, report it by doing .bugreport [Put The error here] without the []`);
-                queue.delete(message.guild.id);
+                client.queue.delete(message.guild.id);
                 return message.channel.send(`There was an error: ${error} If it repeats, report it by doing .bugreport [Put The error here] without the []`);
             }
         } else {
@@ -63,11 +64,11 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
     }
 
     function play(guild, song) {
-        var serverQueue = queue.get(guild.id);
+        var serverQueue = client.queue.get(guild.id);
 
         if (!song) {
             serverQueue.voiceChannel.leave();
-            queue.delete(guild.id);
+            client.queue.delete(guild.id);
             return;
         }
         console.log(serverQueue.songs);
