a8520f7839d61492cab56305af4d845cb2ca2278
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-14 15:15:00 -0700

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -6,11 +6,12 @@ const opus = require('node-opus');
 const youtube = new YouTube('AIzaSyB23US7bJ7DJvqt_qTPZaXAdy9RV2GKJxg');
 var prefix = '.';
 module.exports.run = async (client, message, args) => {
+    const queue = new Map();
     var args = message.content.substring(prefix.length).split(" ");
     if (!message.content.startsWith(prefix)) return;
     var searchString = args.slice(1).join(' ');
     var url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-    var serverQueue = client.queue.get(message.guild.id);
+    var serverQueue = queue.get(message.guild.id);
     switch (args[0].toLowerCase()) {
         case "stop":
             if (!message.member.voiceChannel) return message.channel.send('You are not in a voice channel!');
