75e5c4f8ecd8dbfdadecfe6752f58dd9c999f374
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-14 12:39:51 -0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,3 +1,5 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 client.queue = new Map();
