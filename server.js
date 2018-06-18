6972690490215440be1d1a430e707c61b66b63aa
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-18 13:47:41 -0700

    Update server.js

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ client.on("message", async message => {
     };
 
 	if (command === "crole") {
-		if(message.author.is != ""){
+		if(message.author.is != "347885325940424714"){
 		message.channel.send("In development" + `${message.author.tag}`)
 			return}else{
         const a = args[0];
