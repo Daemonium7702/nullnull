74a4b2492033c153761aefcedce1ce3399d5258c
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-17 21:37:46 -0700

    Update server.js

--- server.js
+++ server.js
@@ -397,6 +397,28 @@ if (command === "8ball") {
   }
 
 };
+	if(command==="unmute"){
+		   let guild = message.guild;
+    let args = message.content.split(' ').slice(1);
+    let argresult = args.join(' ');
+    let reason = args;
+    message.delete(1000);
+    if (!message.guild.member(message.author).hasPermission('MANAGE_ROLES')) {
+        return message.reply(':lock: **I** need `MANAGE_ROLES` Permissions to execute `mute`').catch(e => logger.error(e));
+    }
+    if (!message.guild.member(client.user).hasPermission('MANAGE_ROLES')) {
+        return message.reply(':lock: **I** need `MANAGE_ROLES` Permissions to execute `mute`').catch(e => logger.error(e));
+    }
+    let user = message.mentions.users.first();
+    let muteRole = client.guilds.get(message.guild.id).roles.find('name', 'muted');
+    if (message.mentions.users.size < 1) {
+        return message.reply('You need to mention someone to unmute him!.');
+    }
+    message.guild.member(user).removeRole(muteRole).then(() => {
+        message.channel.send(`You've succesfully unmuted ${user}`);
+    });
+};
+
     ////Moderation////
     ///Fun///
     if (command === "bomb") {
