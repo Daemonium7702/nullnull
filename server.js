5bc8c6a7358a4f0269afcc58e89f174af67db3eb
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-19 00:47:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -177,8 +177,8 @@ client.on("message", async message => {
             "That one guy",
             "My money",
         ]
-        const randomAnswer = answers[Math.floor(Math.random() * answers.length)];
-        const randomNamaste = namaste[Math.floor(Math.random() * answers.length)];
+        const randomAnswer = await answers[Math.floor(Math.random() * answers.length)];
+        const randomNamaste = await namaste[Math.floor(Math.random() * answers.length)];
         m.edit(`Pong! It took ${m.createdTimestamp - message.createdTimestamp}ms to find ***${randomNamaste}*** in ***${randomAnswer}*** after ${Math.round(client.ping)} counts of felony!!`)
 
     };
