94d70341e17bffd4f02cfd27dc7e638e940f076e
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-06-19 22:36:26 -0700

    Update server.js

--- server.js
+++ server.js
@@ -198,7 +198,7 @@ client.on("message", async message => {
 	    if(opttstoIndex == 1){
 		    message.channel.send("You Walk Outside, you will have one quarter the usual time to make your moves until you find safehaven. You may /n 1) stay outside, \n 2) head back inside")
 	  try{
-	  var a = await  message.channel.awaitMessages(message2 => message2.content > 0 && message2.content < 3, {
+	  var a = await message.channel.awaitMessages(message3 => message3.content > 0 && message3.content < 3, {
                     maxMatches: 1,
                     time: 7500,
                     errors: ['time']
@@ -216,7 +216,7 @@ client.on("message", async message => {
 	if(aIndex == 2){
 	message.channel.send("You return inside, the wall of flames draws nearer. You have half the normal time to make your move.  1) End it here and now. \n 2) You head south to the bunker \n 3) You stand where you are by the window staring in awe at the magnificent light. \n respond by typing 1, 2, 3, or 4.")
 	   try{
-	   var aa = await  message.channel.awaitMessages(message2 => message2.content > 0 && message2.content < 3, {
+	   var aa = await  message.channel.awaitMessages(message4 => message4.content > 0 && message4.content < 3, {
                     maxMatches: 1,
                     time: 15000,
                     errors: ['time']
@@ -237,7 +237,7 @@ client.on("message", async message => {
 	message.channel.send("You arrive at the underground concrete bunker entrance.\n 1) You may choose to gather supplies, but your next move will have half the usual time to respond, \n 2)Or you can enter the bunker immediately.");
 	try{
 	
-	 var bb = await  message.channel.awaitMessages(message2 => message2.content > 0 && message2.content < 3, {
+	 var bb = await  message.channel.awaitMessages(message4 => message4.content > 0 && message4.content < 3, {
                     maxMatches: 1,
                     time: 30000,
                     errors: ['time']
