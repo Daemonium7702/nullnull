1cb5e7997497af3eb8f196743503360871e61189
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-08-19 16:56:37 -0700

    Update server.js

--- server.js
+++ server.js
@@ -77,14 +77,12 @@ if(command=="bluebunny"){
 	message.channel.send(bluebunny)
 }
 	    if(command=="broadcast"){
-		    if(message.author.id === "347885325940424714" || "290281424769122305")
+		    if(message.author.id === "347885325940424714" || "290281424769122305"){
     client.guilds.forEach(g => {
-
-      if (g.id !== "475831159209852928") {
   let generalchannel = client.channels.find("name", "general" || "General" || "bot-commands" || "bot-stuff");
-        generalchannel.channel.send = "Hello! I am the creator of DaeBot. I am glad to see you all! This message is to notify you that DaeBot will be undergoing some changes in the future. I would greatly appreciate if you guys could reply to this by doing .bugreport [your server invite]. If you have any suggestions about the bots future, please let me know!"
+        if (!generalchannel) return message.reply("I've detected that this server doesn't have a mod-log channel.");
+	      generalchannel.channel.send = "Hello! I am the creator of DaeBot. I am glad to see you all! This message is to notify you that DaeBot will be undergoing some changes in the future. I would greatly appreciate if you guys could reply to this by doing .bugreport [your server invite]. If you have any suggestions about the bots future, please let me know!"
         //g.defaultChannel.send(text)
-
       }
 
     })
