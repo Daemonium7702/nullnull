5a6ca78fa58ac4579ed7ee3a151692647a5603d1
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-09-15 23:32:40 -0700

    Update server.js

--- server.js
+++ server.js
@@ -501,7 +501,7 @@ scrape().then((value) => {
 anti_spam(client, {
   warnBuffer: 5, //Maximum amount of messages allowed to send in the interval time before getting warned.
   maxBuffer: 10, // Maximum amount of messages allowed to send in the interval time before getting banned.
-  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
+  interval: 500, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
   warningMessage: "Listen here bub. I\'ma abolish you if you dont shut the f up.", // Warning message send to the user indicating they are going to fast.
   banMessage: "has been banned cuz he didn\'t let me get my beauty sleep. Whose next??", // Ban message, always tags the banned user in front of it.
   maxDuplicatesWarning: 5,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
