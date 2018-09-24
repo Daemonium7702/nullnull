fefd6a9de26a79c8169fa0b29b3f97f3c6d0ec5d
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-09-23 17:14:48 -0700

    Create info.js

---
+++ Storage/info.js
@@ -0,0 +1,17 @@
+/*const mongoose= require("mongoose");
+const mySexySchema = mongoose.Schema({
+_id: mongoose.Scheme.Types.ObjectId(),
+username: String,
+bank: String,
+cash: String,
+userid: String,
+})
+module.exports = mongoose.model("Sexy", mySexySchema)
+if(command === "bal"){
+const mongoose = require("mongoose")
+const money = require("../Storage/info.js")
+mongoose.connect("mongodb//:localhost/Sexy")
+	const Money = new money({
+	_id: mongoose.Schema
+	})
+}*/
