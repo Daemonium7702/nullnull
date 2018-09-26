52b7fef9a9f8bd3bd9639d5e958c6ae240652af8
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-09-25 19:27:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -143,9 +143,9 @@ client.on("message", async message => {
 						}
 					}], function(err, result) {
 						assert.equal(err, null);
-						assert.equal(3, result.result.n);
-						assert.equal(3, result.ops.length);
-						console.log("Inserted 3 documents into the collection");
+						assert.equal(1, result.result.n);
+						assert.equal(1, result.ops.length);
+						console.log("Inserted 1 document into the collection");
 						callback(result);
 					})
 
