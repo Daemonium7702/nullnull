c6d5f186850fd2ee49965e2cfaf52a9a4eb58089
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-11-12 20:24:55 -0800

    Update cash.js

--- money/cash.js
+++ money/cash.js
@@ -1,5 +1,5 @@
 	const mongoose = require("mongoose")
-	mongoose.connect('mongodb://DaemoniumAdmin:hippopotomontrosesquippedalaphobia@daemonium-shard-00-00-u2ufm.mongodb.net:27017,daemonium-shard-00-01-u2ufm.mongodb.net:27017,daemonium-shard-00-02-u2ufm.mongodb.net:27017/test?ssl=true&replicaSet=Daemonium-shard-0&authSource=admin&retryWrites=true');
+	mongoose.connect('mongodb://DaemoniumAdmin:Dallasrules123.@daemonium-shard-00-00-u2ufm.mongodb.net:27017,daemonium-shard-00-01-u2ufm.mongodb.net:27017,daemonium-shard-00-02-u2ufm.mongodb.net:27017/test?ssl=true&replicaSet=Daemonium-shard-0&authSource=admin&retryWrites=true');
   const moneySchema = mongoose.Schema({
 		UserId: String,
 		ServerId: String,
