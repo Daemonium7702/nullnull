647d3d12fcaac39335ebaec564f5ae630fde95d0
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-11-12 19:19:53 -0800

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ client.on("message", async message => {
 		}
 	}
 	if (message.author.bot) return;
-	mongoose.connect('mongodb://Dbdbdb:Dallasrules123.@ds235788.mlab.com:35788/daemonium');
+	mongoose.connect('mongodb://DaemoniumAdmin:Dallasrules123.@daemonium-shard-00-00-u2ufm.mongodb.net:27017,daemonium-shard-00-01-u2ufm.mongodb.net:27017,daemonium-shard-00-02-u2ufm.mongodb.net:27017/test?ssl=true&replicaSet=Daemonium-shard-0&authSource=admin&retryWrites=true');
 	if (message.content.indexOf(config.prefix) !== 0){
 		if(message.author.id == '347885325940424714'){
 	const moneySchema = mongoose.Schema({
