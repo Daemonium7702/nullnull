b2e9b66e8d07bd47e2c01f7ab8cb63e09c4f72d9
Daemonium7702 <36465776+Daemonium7702@users.noreply.github.com>
2018-11-13 17:54:54 -0800

    Update server.js

--- server.js
+++ server.js
@@ -53,10 +53,6 @@ client.on("ready", () => {
 	console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 	client.user.setActivity(`on ${client.guilds.size} servers`);
 });
-client.on("guildCreate", guild => {
-	console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-	client.user.setActivity(`on ${client.guilds.size} servers `);
-});
 client.on("guildDelete", guild => {
 	console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
 	client.user.setActivity(`on ${client.guilds.size} servers`);
