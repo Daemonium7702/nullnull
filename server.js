240def5469ab44a33bdf161ffbbbc80bf3741b86
Jjeekkyyllxxhhyyddee <36774748+Jjeekkyyllxxhhyyddee@users.noreply.github.com>
2018-11-21 22:17:46 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-+*/const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client({disableEveryone:true});
 const config = require("./config.json");
 const randomPuppy = require('random-puppy');
